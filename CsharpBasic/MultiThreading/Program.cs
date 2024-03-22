a9b04e09637d161bfd0a9ed63dbacbb429c3f13d
kjw-0301 <jon9831@naver.com>
2024-03-22 09:28:46 +0900

    Review_
    C# 리뷰

---
+++ CsharpBasic/MultiThreading/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MultiThreading
+{
+    internal class Program
+    {
+        const int MB = 1024 * 1024;
+        static void Main(string[] args)
+        {
+            Barista barist1 = HirBarista("J");
+            Barista barist2 = HirBarista("K");
+
+            
+            Thread t1 = new Thread(() =>
+            {
+                HirBarista("J").GoToWork();
+            },1*MB);
+            t1.Start();
+        }
+        static Barista HirBarista(string nickname)
+        {
+            return new Barista(nickname);   
+        }
+    }
+    public enum Beverage
+    {
+        None, 
+        Coffee,
+        Latte,
+        Lemonade,
+        
+    }
+    public class Barista
+    {
+        public Barista(string name) 
+        {
+            Name = name;
+        }
+        
+        public string Name { get; set; }
+        private Random random;
+        
+
+        public void GoToWork()
+        {
+            Console.WriteLine($"바리스타 {Name}은 출근합니다..");
+        }
+        
+        public Beverage MakeRandomBeverage()
+        {
+            return (Beverage)random.Next(1, Enum.GetValues(typeof(Beverage)).Length);
+        }
+    }
+}
---
+++ CsharpBasic/Practice/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice
+{
+    class CharacterController : Object
+    {
+
+    }
+    struct ItemData
+    {
+        public int ID;
+        public int Num;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = new CharacterController();
+            obj = new ItemData { ID = 5, Num = 4 }; //Boxing
+            ItemData data = (ItemData)obj;
+            long a = 10;
+            int n = (int)a; //명시적 형 변환
+            a = n;  //승격 
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

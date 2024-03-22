0fe0d68f99ad73969b6022e51d09c6dc673f02e5
kjw-0301 <jon9831@naver.com>
2024-03-22 12:23:15 +0900

    C# Async
    비동기 함수 수업

---
+++ CsharpBasic/MulitiThreadAsync/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace MulitiThreadAsync
+{
+    internal class Program
+    {
+        //어떤 Task를 취소시키기 위해 신호를 보내는 객체.
+        //Task 할당시 이 source를 통해 Token을 발행해서 줄 수 있고
+        //이 Source의 Cancel() 요청이 발생했을 때, Token을 발행받은 모든 Task를 취소시킬 수 있다.
+
+        static CancellationTokenSource cts;
+        static void Main(string[] args)
+        {
+            cts = new CancellationTokenSource();
+            Task t1 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("~~");
+
+            }, cts.Token, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+            cts.Cancel();
+            if (t1.IsCanceled)
+            {
+                //todo -> Do exception handling
+            }
+
+            // _ 는 반환 내용에 대해 무시하겠다는 명시.
+            //_ = HirBarista("알바").GoToWork().MakeRandomBeverage(); //>무시항목설정
+
+            //C# 버전등의 이슈로 async를 사용할 수 없는 함수에서는 직접 Task 참조를 통해 Wait등을 수행해야한다.
+            //Task<Beverage> task = HirBarista("알바").GoToWork().MakeRandomBeverage();
+            //task.Wait();
+
+            
+            //실행 함수들을 쓰레드풀에서 실행하도록 할당하여 대기열에 등록. >> Token을 발행하여 Task의 실행관리를 할 수 있따.
+            Task.Run(() => HirBarista("알바").GoToWork().MakeRandomBeverage()); 
+            Console.WriteLine("왔으면 일해라!");
+        }
+        static Barista HirBarista(string nickname)
+        {
+            return new Barista(nickname);
+        }
+        public enum Beverage
+        {
+            None,
+            Coffee,
+            Latte,
+            Lemonade,
+
+        }
+        public class Barista
+        {
+            public Barista(string name)
+            {
+                Name = name;
+                random = new Random();
+            }
+
+            public string Name { get; set; }
+
+            private static Dictionary<Beverage, int> s_delayTimes = new Dictionary<Beverage, int>
+            {
+                { Beverage.Coffee, 5000},
+                { Beverage.Lemonade, 6000},
+                { Beverage.Latte, 7000},
+
+            };
+
+
+            private Random random;
+
+
+            public Barista GoToWork()
+            {
+                Console.WriteLine($"바리스타 {Name}은 출근합니다..");
+                return this;
+            }
+
+            public async Task<Beverage> MakeRandomBeverage()
+            {
+                Beverage beverage = (Beverage)random.Next(1, Enum.GetValues(typeof(Beverage)).Length);
+                Console.WriteLine($"바리스타{Name} 은 음료 {beverage}를 제조를 시작했습니다.");
+
+                //Task delayTask = Delay(s_delayTimes[beverage]);
+                //delayTask.Start();
+                //delayTask.Wait();
+
+                await Task.Delay(s_delayTimes[beverage]);
+                //await Delay(s_delayTimes[beverage]); //위 3줄의 코드를 한 줄로 줄이면 다음과 같이 됨.
+                
+                Console.WriteLine($"바리스타{Name} 은 음료 {beverage}를 제조를 완료했습니다.");
+                return beverage;
+            }
+
+            private Task Delay(int ms)
+            {
+                return new Task(() =>
+                {
+                    Thread.Sleep(ms);
+                });
+            }
+        }
+    }
+}
--- CsharpBasic/MultiThreading/Program.cs
+++ CsharpBasic/MultiThreading/Program.cs
@@ -11,10 +11,41 @@
             
             Thread t1 = new Thread(() =>
             {
-                HirBarista("J").GoToWork();
+                HirBarista("J").GoToWork().MakeRandomBeverage(); //함수 체이닝.
             },1*MB);
+            t1.Name = barist1.Name;
+            t1.IsBackground = true; 
             t1.Start();
+            t1.Join(); // Main쓰레드가 t1 쓰레드를 호출.
+
+            Thread.Sleep(2000); //호출한 쓰레드를 2000ms동안 유지? 한 뒤 쓰레드를 닫음.
+            ThreadPool.SetMinThreads(1,0);
+            ThreadPool.SetMaxThreads(4,4); //WorkerTheard는 cpubound task, completion은 IO bounds task.
+
+            Task task1 = new Task(() =>
+            {
+                HirBarista("J").GoToWork().MakeRandomBeverage(); //함수 체이닝.
+
+            }); //관리되는 쓰레드풀에서 알아서 관리해줌(누가? .Net의 가상머신이)
+            task1.Start();
+            task1.Wait(); //task1이 빌려쓰는 쓰레드를 기다리도록 합니다.(여러개의 쓰레드를 한번에 기다리게 할 수 있다.)
+
+            //Task 10개를 배열로 만들어 관리.
+            //★★멀티 쓰레드 환경에서는 Task를 할당하고 실행한 순서대로 출력이 진행된다는 보장이 없다.
+            Task[] tasks = new Task[10];
+            for(int i = 0; i <tasks.Length; i++)
+            {
+                int index = i;
+                tasks[i] = new Task(() =>
+                {
+                    HirBarista($"바리스타{index}").GoToWork().MakeRandomBeverage(); //함수 체이닝
+                });
+                tasks[i].Start();
+            }
+            Task.WaitAll(tasks); //10개의 모든 tasks들이 대기!
         }
+
+
         static Barista HirBarista(string nickname)
         {
             return new Barista(nickname);   
@@ -33,20 +64,24 @@
         public Barista(string name) 
         {
             Name = name;
+            random = new Random();  
         }
         
         public string Name { get; set; }
         private Random random;
         
 
-        public void GoToWork()
+        public Barista GoToWork()
         {
             Console.WriteLine($"바리스타 {Name}은 출근합니다..");
+            return this;
         }
         
         public Beverage MakeRandomBeverage()
         {
-            return (Beverage)random.Next(1, Enum.GetValues(typeof(Beverage)).Length);
+            Beverage beverage = (Beverage)random.Next(1, Enum.GetValues(typeof(Beverage)).Length);
+            Console.WriteLine($"바리스타{Name} 은 음료 +{beverage}를 제조했습니다.");
+            return beverage;
         }
     }
 }
